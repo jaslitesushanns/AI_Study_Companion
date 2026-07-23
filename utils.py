917da1be0027c9d8d31bd7ba21bc5f10a9c18670
jaslitesushanns <jaslitesushanns2026@gmail.com>
2026-07-23 20:30:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,23 @@
+def load_css(file_name):
+    with open(file_name) as f:
+        css = f.read()
 
+    return f"""
+    <style>
+    {css}
+    </style>
+    """
+
+
+
+def clean_text(text):
+    return text.strip()
+
+
+
+def calculate_percentage(completed, total):
+
+    if total == 0:
+        return 0
+
+    return (completed / total) * 100
